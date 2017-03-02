91c9b87e8987c6c4b455ef42e61b2dc05d9fcf56
liyong <liyong@kakatool.com>
2017-03-02 15:15:25 +0800

    Initial Commit

---
+++ WhalePaySDK/WhalePaySDK.m
@@ -0,0 +1,13 @@
+//
+//  WhalePaySDK.m
+//  WhalePaySDK
+//
+//  Created by 李勇 on 2017/3/2.
+//  Copyright © 2017年 liyong. All rights reserved.
+//
+
+#import "WhalePaySDK.h"
+
+@implementation WhalePaySDK
+
+@end
